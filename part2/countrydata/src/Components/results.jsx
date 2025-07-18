55a7971a3a9af06b4b915f8ca9dd805a8575f211
Petteri Raita <petteri.r.raita@gmail.com>
2025-07-18 11:07:31 +0300

    added the weather icon and wind speed. part 2.20 complete.

--- part2/countrydata/src/Components/results.jsx
+++ part2/countrydata/src/Components/results.jsx
@@ -3,7 +3,11 @@ import apiService from "../Services/apiService"
 
 
 const Results = ({ searchTerm, results, setResults, weatherdata, setWeatherdata }) => {
+    const imgURLstart = `https://openweathermap.org/img/wn/`
+    const imgURLend = `@2x.png`
     const [country, setCountry] = useState('')
+    // const [imageURL, setImageURL] = useState('')
+
 
     // NOTE: the last parameter of this useEffect is the dependency array (aka the thing that causes the change and the useeffect to happen)
     useEffect(() => {
@@ -13,7 +17,7 @@ const Results = ({ searchTerm, results, setResults, weatherdata, setWeatherdata 
         }
         if (searchTerm !== '') {
             fetchData()
-            console.log("ran useEffect for searchTerm")
+            // console.log("ran useEffect for searchTerm")
         }
         // Resetting the single country button
         setCountry('')
@@ -33,7 +37,7 @@ const Results = ({ searchTerm, results, setResults, weatherdata, setWeatherdata 
 
     // update the latitude and longitude when results update and there is only 1 country
     useEffect(() => {
-        console.log("ran useEffect for singleCountryView")
+        // console.log("ran useEffect for singleCountryView")
         if (filteredresults.length === 1) {
             const fetchWeather = async () => {
                 const data = await apiService.onSearchWeather(lat, lon)
@@ -44,14 +48,15 @@ const Results = ({ searchTerm, results, setResults, weatherdata, setWeatherdata 
             }
             const lat =(filteredresults[0].latlng[0])
             const lon = (filteredresults[0].latlng[1])
-            console.log("ran useEffect for singleCountryView and ONLY 1 COUNTRY")
-            console.log('filteredresults[0].latlng[0]: ', filteredresults[0].latlng[0]);
-            console.log('filteredresults useEffect: lat: ', lat);
-            console.log('filteredresults useEffect: lon: ', lon);
+            // console.log("ran useEffect for singleCountryView and ONLY 1 COUNTRY")
+            // console.log('filteredresults[0].latlng[0]: ', filteredresults[0].latlng[0]);
+            // console.log('filteredresults useEffect: lat: ', lat);
+            // console.log('filteredresults useEffect: lon: ', lon);
 
             fetchWeather()
-            // console.log('useEffect: weatherdata: ', weatherdata);
+
         }
+
 
     }, [results])
 
@@ -92,14 +97,26 @@ const Results = ({ searchTerm, results, setResults, weatherdata, setWeatherdata 
 
         </>
     )
+
+    // console.log('JSON.stringify(weatherdata): ', JSON.stringify(weatherdata));
+    const imageURL =  (weatherdata.weather && weatherdata.weather.length > 0) ? (
+        // console.log('JSON.stringify(weatherdata): ', JSON.stringify(weatherdata.weather));
+        // console.log('JSON.stringify(weatherdata): ', JSON.stringify(weatherdata.weather[0]));
+        imgURLstart + weatherdata.weather[0].icon + imgURLend
+        // console.log('imageURL: ', imageURL);
+    ) : ''
     const WeatherProp = ({ c, weather }) => (
         <div>
             <h2>Weather in {c.capital}</h2>
-            { !weather?.main ? (
+            { !weather?.main || !weatherdata?.weather ? (
                 <p>loading weather...</p>
             ) : (
                 <>
                     <div>Temperature: {weather.main.temp} °C</div>
+                    <img
+                        src={imageURL}
+                    />
+                    <div>Wind: {weather.wind.speed} m/s</div>
                 </>
             )}
         </div>
@@ -114,7 +131,7 @@ const Results = ({ searchTerm, results, setResults, weatherdata, setWeatherdata 
         return `too many matches, specify more characters`
     }
     else if (filteredresults.length === 1) {
-        console.log('filteredresults[0].name.common: ', filteredresults[0].name.common);
+        // console.log('filteredresults[0].name.common: ', filteredresults[0].name.common);
         const c = filteredresults[0]
 
         return <SingleCountry c={c} weather = {weatherdata}/>
@@ -122,7 +139,7 @@ const Results = ({ searchTerm, results, setResults, weatherdata, setWeatherdata 
     else {
         const filteredresultsMapped = filteredresults.map(c =>
         (<div key={c.name.common}> {c.name.common}  <button onClick={() => {
-            console.log("Country set")
+            // console.log("Country set")
             setCountry(c.name.common)
         }}
         > show </button> </div>)
