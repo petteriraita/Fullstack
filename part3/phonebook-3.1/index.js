d3123096698ef34ad7be3586b67d598848268758
Petteri Raita <petteri.r.raita@gmail.com>
2025-08-03 10:57:03 +0300

    phonebook 3.3 done

--- part3/phonebook-3.1/index.js
+++ part3/phonebook-3.1/index.js
@@ -24,11 +24,43 @@ const persons = [
       "number": "39-23-6423122"
     }
 ]
+app.get('/test/:id', (req, res) => {
+  res.send(`You gave me id: ${req.params.id}`)
+})
+
 
 app.get('/api/persons', (request, response) => {
     return response.json(persons)
 }
 )
+app.get('/api/persons/:num', (request, response) => {
+  const num = request.params.num
+  // console.log('got the num ', num);
+  
+  // // you cannot send 2 responses, since those flush the headers and they cant bbe changed
+  // // response.send('<p>some html</p>')
+
+  // persons.forEach( p => {
+  //   console.log('p.id: ', p.id);
+  //   console.log('p.name: ', p.name);
+    
+  // })
+  // console.log('persons.length: ', persons.length);
+  const filteredPersons = persons.filter(p => p.id === num)
+  console.log('filteredPersons.length: ', filteredPersons.length);
+  if (filteredPersons.length === 1) {
+        return response.json(filteredPersons)
+  
+  }
+  else {
+    return response.status(404).json({
+      error:'the id number didnt fit any single person'
+    })
+  }
+  // response.send(`You gave me the id: ${request.params.num}`)
+})
+
+
 app.get('/info', (request, response) => {
   const curdate = new Date().toString()
   const nrofpeople = persons.length
@@ -46,3 +78,4 @@ app.get('/info', (request, response) => {
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+
